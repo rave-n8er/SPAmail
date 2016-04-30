3b5bfccca2060871eb5a8c1f1acf3c70d881edde
rave-n8er <sean@innova-seans.co.za>
2016-04-30 20:43:00 +0200

    Final upload
    Uploading finished solution

---
+++ SPAmail/app/app.module.js
@@ -0,0 +1,51 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('SPAmailApp').controller('getMails', function ($scope, $http) {
+
+        // gets emails from json.  defaults to Inbox.
+        // CANNOT MOVE THIS OUT OF HERE TO ANOTHER FUNCTION - 
+        // $scope becomes unavailable, despite being passed in, 
+        // and no data can be returned from the anonymous 'success' function!
+        $http.get('emails.json').success(function (data) {
+
+            $scope.emails = data.inboxMails;
+
+        });
+
+    });
+
+
+    angular.module('SPAmailApp').controller('changeBox', function ($scope, $http) {
+
+        // Just for this event.
+        $scope.boxType = function (boxtype) {
+
+            // gets emails from json. Could be a webservice...
+            // CANNOT MOVE THIS OUT OF HERE TO ANOTHER FUNCTION - 
+            // $scope becomes unavailable, despite being passed in, 
+            // and no data can be returned from the anonymous 'success' function!
+            $http.get('emails.json').success(function (data) {
+
+                switch (boxtype) {
+                    case 'DR':
+                        $scope.emails = data.draftMails;
+                        break;
+                    case 'IN':
+                        $scope.emails = data.inboxMails;
+                        break;
+                    case 'SE':
+                        $scope.emails = data.sentMails;
+                        break;
+                    default:
+                        $scope.emails = null;
+                }
+            });
+        }
+
+    });
+
+})();
+
+
+
---
+++ SPAmail/app/services/services.js
@@ -0,0 +1,26 @@
+﻿
+
+
+// can never use this - impossible to return data out of the anonymous function;
+// $scope is empty at this level, too.
+function getMyEmails(boxType, $scope, $http) {
+
+    // gets emails from json. Could be a webservice...
+    $http.get('emails.json').success(function (data) {
+
+        switch (boxType) {
+            case 'DR':
+                $scope.emails = data.draftMails;
+                break;
+            case 'IN':
+                $scope.emails = data.inboxMails;
+                break;
+            case 'SE':
+                $scope.emails = data.sentMails;
+                break;
+            default:
+                $scope.emails = null;
+        }
+    });
+
+}
